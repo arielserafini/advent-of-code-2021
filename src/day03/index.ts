cecd0e0a44762830768b6c5ad7fe8a76184e8a36
Ariel Serafini <git@arielserafini.com>
2021-12-13 01:40:22 -0300

    day 3

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -40,10 +40,73 @@ const part1 = (rawInput: string) => {
   return result;
 };
 
+type Bit = 1 | 0;
+
+function getMostCommonBitForInputPosition(input: string[], position: number) {
+  let onesCount = 0;
+
+  const bits = input.map((val) => {
+    const bit = parseInt(val[position], 10) as Bit;
+    if (bit === 1) {
+      onesCount++;
+    }
+    return bit;
+  });
+  const mostCommonBit = onesCount >= bits.length / 2 ? 1 : 0;
+
+  return mostCommonBit;
+}
+
+function getMostCommonBitsForInput(input: string[]) {
+  const values = new Array(input[0].length).fill(0);
+
+  return values.map((_, index) =>
+    getMostCommonBitForInputPosition(input, index),
+  );
+}
+
+function getOxygenRating(input: string[]) {
+  let oxy = [...input];
+  let pass = 0;
+  let currentPos = 0;
+
+  while (oxy.length > 1 && pass < 10) {
+    pass++;
+    const commonBits = getMostCommonBitsForInput(oxy);
+
+    oxy = oxy.filter((val) => parseInt(val[currentPos], 10) === commonBits[currentPos]);
+    currentPos++;
+  }
+
+  return oxy[0];
+}
+
+function getCO2Rating(input: string[]) {
+  let co2 = [...input];
+  let pass = 0;
+  let currentPos = 0;
+
+  while (co2.length > 1 && pass < 10) {
+    pass++;
+    const commonBits = getMostCommonBitsForInput(co2);
+
+    co2 = co2.filter((val) => parseInt(val[currentPos], 10) !== commonBits[currentPos]);
+    currentPos++;
+  }
+
+  return co2[0];
+}
+
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+
+
+  const oxygenGeneratorRating = parseInt(getOxygenRating(input), 2);
+  const co2ScrubberRating = parseInt(getCO2Rating(input), 2);
+  const lifeSupportRating = oxygenGeneratorRating * co2ScrubberRating;
+
+  return lifeSupportRating;
 };
 
 run({
@@ -64,10 +127,10 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: testInput,
+        expected: 230,
+      },
     ],
     solution: part2,
   },
